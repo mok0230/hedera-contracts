24854902e5a9e72d216a06245da9b4d322209fdc
mok <mok0230@protonmail.com>
2022-05-25 21:47:26 -0700

    update phone-number deploy script to use config script

--- phone-number-contract/deploy-contract.js
+++ phone-number-contract/deploy-contract.js
@@ -1,23 +1,16 @@
-// console.clear();
-require("dotenv").config();
 const {
-  AccountId,
-  PrivateKey,
-  Client,
   FileCreateTransaction,
   ContractCreateTransaction,
   ContractFunctionParameters,
-  ContractExecuteTransaction,
   ContractCallQuery,
   Hbar
 } = require("@hashgraph/sdk");
-const fs = require("fs");
+const { getOperatorConfig } = require('./../utils/operator');
+const fs = require('fs');
+
+const { operatorPrivateKey, client } = getOperatorConfig();
 
 // Configure accounts and client
-const operatorId = AccountId.fromString(process.env.OPERATOR_ID);
-const operatorKey = PrivateKey.fromString(process.env.OPERATOR_PVKEY);
-
-const client = Client.forTestnet().setOperator(operatorId, operatorKey);
 
 async function main() {
   // Import the compiled contract bytecode
@@ -27,10 +20,10 @@ async function main() {
   // Create a file on Hedera and store the bytecode
   const fileCreateTx = new FileCreateTransaction()
     .setContents(contractBytecode)
-    .setKeys([operatorKey])
-    .setMaxTransactionFee(new Hbar(0.75))
+    .setKeys([operatorPrivateKey])
+    .setMaxTransactionFee(new Hbar(0.9))
     .freezeWith(client);
-  const fileCreateSign = await fileCreateTx.sign(operatorKey);
+  const fileCreateSign = await fileCreateTx.sign(operatorPrivateKey);
   const fileCreateSubmit = await fileCreateSign.execute(client);
   const fileCreateRx = await fileCreateSubmit.getReceipt(client);
   const bytecodeFileId = fileCreateRx.fileId;
@@ -65,40 +58,10 @@ async function main() {
       "getMobileNumber",
       new ContractFunctionParameters().addString("Alice")
     )
-    .setMaxQueryPayment(new Hbar(0.00000000001));
   const contractQuerySubmit = await contractQueryTx.execute(client);
   const contractQueryResult = contractQuerySubmit.getUint256(0);
   console.log(
     `- Here's the phone number you asked for: ${contractQueryResult} \n`
   );
-
-  // // Call contract function to update the state variable
-  // const contractExecuteTx = new ContractExecuteTransaction()
-  //   .setContractId(contractId)
-  //   .setGas(100000)
-  //   .setFunction(
-  //     "setMobileNumber",
-  //     new ContractFunctionParameters().addString("Bob").addUint256(222222)
-  //   )
-  //   .setMaxTransactionFee(new Hbar(0.75));
-  // const contractExecuteSubmit = await contractExecuteTx.execute(client);
-  // const contractExecuteRx = await contractExecuteSubmit.getReceipt(client);
-  // console.log(
-  //   `- Contract function call status: ${contractExecuteRx.status} \n`
-  // );
-
-  // // Query the contract to check changes in state variable
-  // const contractQueryTx1 = new ContractCallQuery()
-  //   .setContractId(contractId)
-  //   .setGas(100000)
-  //   .setFunction(
-  //     "getMobileNumber",
-  //     new ContractFunctionParameters().addString("Bob")
-  //   );
-  // const contractQuerySubmit1 = await contractQueryTx1.execute(client);
-  // const contractQueryResult1 = contractQuerySubmit1.getUint256(0);
-  // console.log(
-  //   `- Here's the phone number you asked for: ${contractQueryResult1} \n`
-  // );
 }
 main();
