3e1b04644b4b015c08821ed52a0e1e645be897ed
mok <mok0230@protonmail.com>
2022-06-28 09:46:31 -0700

    add hedera network to hardhat config

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,11 @@
-/**
- * @type import('hardhat/config').HardhatUserConfig
- */
+require('dotenv').config();
+
 module.exports = {
   solidity: "0.7.3",
+  networks: {
+    hedera: {
+      url: 'https://testnet.hashio.io/api',
+      accounts: [`0x${process.env.ALT_OPERATOR_PRIVATE_KEY}`],
+    }
+  },
 };
