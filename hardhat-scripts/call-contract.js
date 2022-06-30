fa8499008dfb025d92781104ae4a6ac0daef5603
mok <mok0230@protonmail.com>
2022-06-30 14:41:55 -0700

    update call-contract and send-funds scripts with new Wallet signer instantiation and more logging

--- hardhat-scripts/call-contract.js
+++ hardhat-scripts/call-contract.js
@@ -1,18 +1,63 @@
+const { computeAliasFromPubKey } = require('@hethers/transactions');
+const { Wallet } = require('ethers');
 const { ethers } = require('hardhat');
 const testSolidityFeaturesAbi = require('../test-solidity-features/SolidityFeaturesContract_sol_SolidityFeaturesContract.json')
 require('dotenv').config({ path: '../.env' })
 
 const TEST_SOLIDITY_FEATURES = '0x000000000000000000000000000000000214db10';
+const NUM = 1;
 
 async function main() {
-    console.log('testSolidityFeaturesAbi', testSolidityFeaturesAbi)
-    const contract = new ethers.Contract(TEST_SOLIDITY_FEATURES, testSolidityFeaturesAbi);
+    // script hangs while getting provider when signer is instantiated with ethers.getSigners
+    // const [signer] = await ethers.getSigners();
 
-    const [signer] = await ethers.getSigners();
+    // script creates the transaction when signer is instantiated with new Wallet() but 
+    // eventually fails with error in while awaiting tx.wait():
+    // UnhandledPromiseRejectionWarning: Error: invalid BigNumber value (argument="value", value=null, code=INVALID_ARGUMENT, version=bignumber/5.6.2)
+    const signer = new Wallet(process.env.ETHERS_PRIVATE_KEY, ethers.provider);
 
-    const tx = await contract.connect(signer).updateTestMapping('Bob', 1);
+    const txCount = await ethers.provider.getTransactionCount(signer.address);
 
-    console.log('tx', tx)
+    console.log('txCount', txCount);
+
+    const contract = new ethers.Contract(TEST_SOLIDITY_FEATURES, testSolidityFeaturesAbi, signer);
+
+    const currentNum = await contract.getTestMapping('Bob');
+
+    console.log('currentNum', currentNum);
+    
+    const tx = await contract.updateTestMapping('Bob', NUM);
+
+    console.log('tx', tx);
+    // sample log:
+    // {
+    //     type: 2,
+    //     chainId: 296,
+    //     nonce: 39,
+    //     maxPriorityFeePerGas: BigNumber { _hex: '0x59682f00', _isBigNumber: true },
+    //     maxFeePerGas: BigNumber { _hex: '0x59682f00', _isBigNumber: true },
+    //     gasPrice: null,
+    //     gasLimit: BigNumber { _hex: '0x061a80', _isBigNumber: true },
+    //     to: '0x000000000000000000000000000000000214db10',
+    //     value: BigNumber { _hex: '0x00', _isBigNumber: true },
+    //     data: '0x06e8fac2000000000000000000000000000000000000000000000000000000000000004000000000000000000000000000000000000000000000000000000000000000010000000000000000000000000000000000000000000000000000000000000003426f620000000000000000000000000000000000000000000000000000000000',
+    //     accessList: [],
+    //     hash: '0xc6fea8c1db52a0e0bff4ac7451133dc5eded94e148a8ef97cfe46fcf120e7f63',
+    //     v: 1,
+    //     r: '0xf758dc7d2ac1d0b137511772e8490aa0f31f7ce97a914a4fc1c1cba4c3ae8287',
+    //     s: '0x50ee41409fbca965e66102fc95b42c6efea13214f5a3392ec7ea96f439ebed7a',
+    //     from: '0x205399A519D341051E86e790AaA0F713340745CA',
+    //     confirmations: 0,
+    //     wait: [Function]
+    //   }
+
+    await tx.wait();
+
+    const numAfterUpdate = await contract.getTestMapping('Bob');
+
+    console.log('numAfterUpdate', numAfterUpdate);
+
+    console.log('done');
 }
 
 main();
--- hardhat-scripts/send-funds.js
+++ hardhat-scripts/send-funds.js
@@ -1,3 +1,4 @@
+const { BigNumber } = require('ethers');
 const { ethers } = require('hardhat');
 require('dotenv').config({ path: '../.env' })
 
@@ -8,25 +9,53 @@ console.log('ACCOUNT_1', ACCOUNT_1)
 console.log('ACCOUNT_2', ACCOUNT_2)
 
 async function main() {
-    // console.log('testSolidityFeaturesAbi', testSolidityFeaturesAbi)
+    // script hangs while getting provider when signer is instantiated with ethers.getSigners
+    // const [signer] = await ethers.getSigners();
 
-    const [signer] = await ethers.getSigners();
-
-    // const balance = await ethers.provider.getBalance(process.env.ETHERS_EVM_ADDRESS_FROM_KEY);
-
-    // console.log('balance', balance)
+    // script creates the transaction when signer is instantiated with new Wallet() but 
+    // eventually fails with error in while awaiting tx.wait():
+    // UnhandledPromiseRejectionWarning: Error: invalid BigNumber value (argument="value", value=null, code=INVALID_ARGUMENT, version=bignumber/5.6.2)
+    const signer = new ethers.Wallet(process.env.ETHERS_PRIVATE_KEY, ethers.provider);
 
     const balance = await ethers.provider.getBalance(signer.address);
 
     console.log('signer balance', balance);
 
+    const txCount = await ethers.provider.getTransactionCount(signer.address);
+
+    console.log('txCount', txCount);
+
     const tx = await signer.sendTransaction({
-        from: `0x${ACCOUNT_1}`,
         to: `0x${ACCOUNT_2}`,
-        value: '1'
+        value: BigNumber.from(10000),
     });
 
-    console.log('tx', tx)
+    console.log('tx', tx);
+    // sample log:
+    // {
+    //     type: 2,
+    //     chainId: 296,
+    //     nonce: 38,
+    //     maxPriorityFeePerGas: BigNumber { _hex: '0x59682f00', _isBigNumber: true },
+    //     maxFeePerGas: BigNumber { _hex: '0x59682f00', _isBigNumber: true },
+    //     gasPrice: null,
+    //     gasLimit: BigNumber { _hex: '0x5208', _isBigNumber: true },
+    //     to: '0xAc058B91eD2618512a5a012CD5766AFCC6B630eE',
+    //     value: BigNumber { _hex: '0x2710', _isBigNumber: true },
+    //     data: '0x',
+    //     accessList: [],
+    //     hash: '0x002dbe2f12714960459a15429c0fc0aeb678eb890c7bb633910af461c3bd17c2',
+    //     v: 0,
+    //     r: '0x7216e198a1b0b4aacc9ab8b1af4d4702f199fff9b1da714c1dc92792690c9918',
+    //     s: '0x1fd43a5454c214e27abfd511daa5a55bfd8111e1cd2dd57dd1aa7e45c9d5f7b4',
+    //     from: '0x205399A519D341051E86e790AaA0F713340745CA',
+    //     confirmations: 0,
+    //     wait: [Function]
+    //   }
+
+    await tx.wait();
+
+    console.log('done');
 }
 
 main();
